b747ab5d79ae3101248a44e18e3fe4188a02e16e
NaviLeiru <sejismundoi123@gmail.com>
2024-07-25 15:27:37 -0800

    layout sidebar login reflect name

---
+++ src/components2/Services/userServices.jsx
@@ -0,0 +1,18 @@
+import { db } from '/backend/firebase';
+import { doc, getDoc } from 'firebase/firestore';
+
+export const getUserDetails = async (userId) => {
+    try {
+        const userRef = doc(db, 'users', userId);
+        const userDoc = await getDoc(userRef);
+        if (userDoc.exists()) {
+            return userDoc.data();
+        } else {
+            console.log('No such document!');
+            return null;
+        }
+    } catch (error) {
+        console.error("Error fetching user details: ", error);
+        return null;
+    }
+};
