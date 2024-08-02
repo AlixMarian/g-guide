74fcd7ae940c4bddc86da766dfcfabb7a4e0b4c9
AlixMarian <alixmarian.gonzaga.20@usjr.edu.ph>
2024-08-02 21:16:33 +0800

    can create + disable + delete + edit timeslots

--- src/components2/Slots.jsx
+++ src/components2/Slots.jsx
@@ -20,13 +20,14 @@ export const Slots = () => {
   const [filteredSlots, setFilteredSlots] = useState([]);
   const [editing, setEditing] = useState(false);
   const [currentSlotId, setCurrentSlotId] = useState(null);
+  const [selectedStatus, setSelectedStatus] = useState('');
 
   const auth = getAuth();
 
   const handleRecurringChange = (e) => {
     setIsRecurring(e.target.checked);
     if (!e.target.checked) {
-      setEndDate(''); // Clear end date if not recurring
+      setEndDate('');
     }
   };
 
@@ -56,11 +57,19 @@ export const Slots = () => {
   };
 
   const convertTo12HourFormat = (time) => {
-    if (!time) return '';
+    if (!time || time === "none") return "none";
     const [hours, minutes] = time.split(':');
     let hours12 = (hours % 12) || 12;
     const ampm = hours >= 12 ? 'PM' : 'AM';
     return `${hours12}:${minutes} ${ampm}`;
+  };
+
+  const renderTime = (slot) => {
+    if (!slot.startTime || !slot.endTime || 
+      slot.startTime === "none" || slot.endTime === "none") {
+      return 'Information unavailable: Date disabled';
+    }
+    return `${convertTo12HourFormat(slot.startTime)} - ${convertTo12HourFormat(slot.endTime)}`;
   };
 
   const handleCreateSlots = async (e) => {
@@ -129,19 +138,19 @@ export const Slots = () => {
   };
 
   const filterSlotsByDate = useCallback(() => {
+    let filtered = slots;
     if (selectedDate) {
-      const filtered = slots.filter(slot => 
-        new Date(slot.startDate).toDateString() === selectedDate.toDateString()
-      );
-      setFilteredSlots(filtered);
-    } else {
-      setFilteredSlots(slots);
+      filtered = filtered.filter(slot => new Date(slot.startDate).toDateString() === selectedDate.toDateString());
     }
-  }, [selectedDate, slots]);
-
+    if (selectedStatus) {
+      filtered = filtered.filter(slot => slot.slotStatus === selectedStatus);
+    }
+    setFilteredSlots(filtered);
+  }, [selectedDate, slots, selectedStatus]);
+  
   useEffect(() => {
     filterSlotsByDate();
-  }, [selectedDate, slots, filterSlotsByDate]);
+  }, [selectedDate, slots, selectedStatus, filterSlotsByDate]);
 
 
     const handleEditSlot = (slot) => {
@@ -172,7 +181,7 @@ export const Slots = () => {
           try {
             await updateDoc(doc(db, 'slot', currentSlotId), {
               startDate,
-              endDate: isRecurring ? endDate : '', // Only update endDate if recurring
+              endDate: isRecurring ? endDate : '', 
               startTime,
               endTime,
               slotStatus: 'active',
@@ -187,7 +196,7 @@ export const Slots = () => {
             setEditing(false);
             setCurrentSlotId(null);
             toast.success('Slot updated successfully');
-            fetchSlots(); // Fetch the updated slots list
+            fetchSlots(); 
           } catch (error) {
             toast.error('Error updating slot: ', error);
           }
@@ -196,19 +205,149 @@ export const Slots = () => {
         }
       };
 
+      const handleDisableSlots = async (e) => {
+        e.preventDefault();
+        const user = auth.currentUser;
+      
+        if (user) {
+          try {
+            const start = new Date(startDate);
+            const end = new Date(endDate);
+            const dates = [];
+            let currentDate = start;
+      
+            while (currentDate <= end) {
+              dates.push(new Date(currentDate));
+              currentDate.setDate(currentDate.getDate() + 1);
+            }
+      
+            
+            const slotsCollection = collection(db, 'slot');
+            const existingDisabledSlotsPromises = dates.map(async (date) => {
+              const formattedDate = date.toISOString().split('T')[0];
+              const q = query(
+                slotsCollection,
+                where('startDate', '==', formattedDate),
+                where('slotStatus', '==', 'disabled'),
+                where('churchId', '==', user.uid)
+              );
+              const querySnapshot = await getDocs(q);
+              return querySnapshot.docs.length > 0;
+            });
+      
+            const existingDisabledSlots = await Promise.all(existingDisabledSlotsPromises);
+      
+            
+            const datesToDisable = dates.filter((_, index) => !existingDisabledSlots[index]);
+      
+            if (datesToDisable.length > 0) {
+              const promises = datesToDisable.map(async (date) => {
+                const formattedDate = date.toISOString().split('T')[0];
+                await addDoc(collection(db, 'slot'), {
+                  startDate: formattedDate,
+                  startTime: 'none',
+                  endTime: 'none',
+                  slotStatus: 'disabled',
+                  churchId: user.uid,
+                });
+              });
+      
+              await Promise.all(promises);
+              
+            } else {
+              await addDoc(collection(db, 'slot'), {
+                startDate,
+                startTime: 'none',
+                endTime: 'none',
+                slotStatus: 'disabled',
+                churchId: user.uid,
+              });
+            }
+      
+            setStartDate('');
+            setEndDate('');
+            setStartTime('');
+            setEndTime('');
+            setIsRecurring(false);
+            toast.success('Date(s) have been disabled');
+            fetchSlots();
+          } catch (error) {
+            toast.error('Error disabling slots: ', error);
+          }
+        } else {
+          toast.error('No user signed in.');
+        }
+      };
+      
+      const handleDisableSelectedSlot = async () => {
+        const user = auth.currentUser;
+    
+        if (user && currentSlotId) {
+          try {
+            await updateDoc(doc(db, 'slot', currentSlotId), {
+              slotStatus: 'disabled',
+              startTime: 'none',
+              endTime: 'none',
+            });
+    
+            setStartDate('');
+            setEndDate('');
+            setStartTime('');
+            setEndTime('');
+            setIsRecurring(false);
+            setEditing(false);
+            setCurrentSlotId(null);
+            toast.success('Slot disabled successfully');
+            fetchSlots();
+          } catch (error) {
+            toast.error('Error disabling slot: ', error);
+          }
+        } else {
+          toast.error('No user signed in or no slot selected.');
+        }
+      };
+
+      const handleStatusChange = (status) => {
+        if (status === 'all') {
+          setSelectedStatus('');
+        } else {
+          setSelectedStatus(status);
+        }
+      };
+
       return (
         <div>
           <div className='card'>
             <div className='card-body'>
-              <div className='filterDates'>
-                <label className='me-2'>Filter by date:</label>
-                <DatePicker
-                  className='form-control'
-                  selected={selectedDate}
-                  onChange={date => setSelectedDate(date)}
-                  showYearDropdown
-                />
+            <div className="filtering">
+              <div className="row">
+                <div className="col-md-6 mb-3">
+                  <div className='filterDates'>
+                    <label className='me-2'><b>Filter by date:</b></label>
+                    <DatePicker
+                      className='form-control'
+                      selected={selectedDate}
+                      onChange={date => setSelectedDate(date)}
+                      showYearDropdown
+                    />
+                  </div>
+                </div>
+
+                <div className="col-md-6 mb-3">
+                  <div className="filterStatus dropdown">
+                    <label className='me-2'><b>Filter by status:</b></label>
+                    <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    {selectedStatus || 'Select Status'}
+                    </button>
+                    <ul className="dropdown-menu">
+                    <li><a className="dropdown-item" href="#" onClick={(e) => { e.preventDefault(); handleStatusChange('all'); }}>All</a></li>
+                    <li><a className="dropdown-item" href="#" onClick={(e) => { e.preventDefault(); handleStatusChange('active'); }}>Active Dates</a></li>
+                      <li><a className="dropdown-item" href="#" onClick={(e) => { e.preventDefault(); handleStatusChange('disabled'); }}>Disabled Dates</a></li>
+                    </ul>
+                  </div>
+                </div>
               </div>
+            </div>
               <br />
               <h5>Created Time Slots</h5>
               <table className="table">
@@ -216,27 +355,29 @@ export const Slots = () => {
                   <tr>
                     <th scope='col'>Date</th>
                     <th scope='col'>Time</th>
+                    <th scope='col'>Status</th>
                     <th></th>
                     <th></th>
                   </tr>
                 </thead>
                 <tbody>
-                  {filteredSlots.map(slot => (
-                    <tr key={slot.id}>
-                      <td>{slot.startDate}</td>
-                      <td>{convertTo12HourFormat(slot.startTime)} - {convertTo12HourFormat(slot.endTime)}</td>
-                      <td>
-                        <button className="btn btn-primary me-2" onClick={() => handleEditSlot(slot)}>
-                          Edit
-                        </button>
-                      </td>
-                      <td>
-                        <button className="btn btn-danger" onClick={() => handleDeleteSlot(slot.id)}>
-                          Delete
-                        </button>
-                      </td>
-                    </tr>
-                  ))}
+                {filteredSlots.map(slot => (
+                <tr key={slot.id}>
+                  <td>{slot.startDate}</td>
+                  <td>{renderTime(slot)}</td>
+                  <td>{slot.slotStatus}</td>
+                  <td>
+                    <button className="btn btn-primary me-2" onClick={() => handleEditSlot(slot)}>
+                      Edit
+                    </button>
+                  </td>
+                  <td>
+                    <button className="btn btn-danger" onClick={() => handleDeleteSlot(slot.id)}>
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              ))}
                 </tbody>
               </table>
               <br />
@@ -257,33 +398,37 @@ export const Slots = () => {
                         Recurring Time Slot?
                       </label>
                     </div>
-                    <div className='col-md-6'>
-                      <div className="mb-3">
-                        <label htmlFor="startDate" className="form-label"><b>Start Date</b></label>
-                        <input
-                          type="date"
-                          className="form-control"
-                          id="startDate"
-                          value={startDate}
-                          onChange={(e) => setStartDate(e.target.value)}
-                          required
-                        />
+                    
+                      <div className="col-md-6">
+                        <div className="mb-3">
+                          <label htmlFor="startDate" className="form-label"><b>Start Date</b></label>
+                          <input
+                            type="date"
+                            className="form-control"
+                            id="startDate"
+                            value={startDate}
+                            onChange={(e) => setStartDate(e.target.value)}
+                            required
+                          />
+                        </div>
                       </div>
-                    </div>
-                    <div className='col-md-6'>
-                      <div className="mb-3">
-                        <label htmlFor="endDate" className="form-label"><b>End Date</b> <i>(Only for recurring slots)</i></label>
-                        <input
-                          type="date"
-                          className="form-control"
-                          id="endDate"
-                          value={endDate}
-                          onChange={(e) => setEndDate(e.target.value)}
-                          readOnly={!isRecurring}
-                          required
-                        />
+
+                      <div className="col-md-6 ">
+                        <div className="mb-3">
+                          <label htmlFor="endDate" className="form-label"><b>End Date</b> <i>(Only for recurring slots)</i></label>
+                          <input
+                            type="date"
+                            className="form-control"
+                            id="endDate"
+                            value={endDate}
+                            onChange={(e) => setEndDate(e.target.value)}
+                            readOnly={!isRecurring}
+                            required
+                          />
+                        </div>
                       </div>
-                    </div>
+                    
+
                     <div className='col-md-6'>
                       <div className="mb-3">
                         <label htmlFor="startTime" className="form-label"><b>Start Time</b></label>
@@ -313,11 +458,22 @@ export const Slots = () => {
                     <div className='col-12 mt-3'>
                       <div className="d-flex justify-content-end gap-2">
                         <button type="submit" className="btn btn-success">
-                          {editing ? 'Update Slot' : 'Confirm Change'}
+                        {editing ? 'Update Time Slot' : 'Create Time Slot'}
                         </button>
                         {editing && (
+                          <>
                           <button type="button" className="btn btn-secondary" onClick={handleCancelEdit}>
                             Cancel
+                          </button>
+                          <button type="button" className="btn btn-warning" onClick={handleDisableSelectedSlot}>
+                            Disable Selected Slot
+                          </button>
+                        </>
+
+                        )}
+                        {!editing && (
+                          <button type="button" className="btn btn-danger" onClick={handleDisableSlots}>
+                            Disable Date
                           </button>
                         )}
                         <button type="reset" className="btn btn-danger">Clear</button>
