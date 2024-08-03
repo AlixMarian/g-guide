70580b52231a7f952792b84e4880cdbb8631f000
NaviLeiru <sejismundoi123@gmail.com>
2024-08-03 00:51:56 -0800

    appointments tab fix

--- src/components2/Appointments.jsx
+++ src/components2/Appointments.jsx
@@ -10,6 +10,7 @@ export const Appointments = () => {
     const [showModal, setShowModal] = useState(false);
     const [smsContent, setSmsContent] = useState('');
     const [startDate, setStartDate] = useState(new Date()); 
+    const [formattedDate, setFormattedDate] = useState('');
 
     const handleShowModal = () => setShowModal(true);
     const handleCloseModal = () => setShowModal(false);
@@ -28,178 +29,115 @@ export const Appointments = () => {
         return date.toLocaleDateString(undefined, options);
     };
 
+    const handleDateChange = (date) => {
+        setStartDate(date);
+        setFormattedDate(formatDate(date));
+    };
+
     return (
         <>
             <div className="appoinmentsPage"></div>
             <h1>Appointments</h1>
-            <div className="serviceFiltering">
-                <label htmlFor="dateSelect" className="form-label">
-                    Select a date: 
-                    <DatePicker 
-                        selected={selectedDate} 
-                        onChange={date => setSelectedDate(date)}
-                        showYearDropdown
-                    />
-                </label>
-            </div>
 
-            <div className="appointmentsView">
-                {/*<div className="slots">
-                   <h1>{selectedDate ? ` ${formatDate(selectedDate)}` : 'No date selected'}</h1> 
-                </div> */}      
-                <div className="pendingAppointments">
-                <div className="titleFilter">
-                    <h2>Pending Appointments</h2>
-                    <div className="btn-group">
-                    <button type="button" className="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-                        Filter
-                    </button>
-                    <ul className="dropdown-menu">
-                        <li><a className="dropdown-item" href="#">Date</a></li>
-                        <li><a className="dropdown-item" href="#">Purpose</a></li>
-                    </ul>
-                </div>
-                </div>
-                    <table className="table">
-                    <thead>
-                        <tr>
-                            <th scope="col">Date</th>
-                            <th scope="col">Time</th>
-                            <th scope="col">Period</th>
-                            <th scope="col">Purpose</th>
-                            <th scope="col">Requested by:</th>
-                            <th scope="col">More Info</th> 
-                        </tr>
-                    </thead>
-                    <tbody> 
-                    {pendingAppointments.map((appointment, index) => (
-                        <tr>
-                            <td>
-                                11/22/21
-                                {/*massSchedules.massDate*/}
-                            </td>
-                            <td>
-                                1:00
-                                {/*massSchedules.massDate*/}
-                            </td>
-                            <td>
-                                Pm
-                                {/*massSchedules.massDate*/}
-                            </td>
-                            <td>
-                                Burial
-                                {/*massSchedules.massDate*/}
-                            </td>
-                            <td>
-                                Navia Caspar
-                                {/*massSchedules.massDate*/}
-                            </td>
-                            <td>
-                                <button type="button" class="btn btn-info">Info</button>
-                            </td>
-                        </tr>
-                    ))}
-
-                    </tbody>
-                    </table>
-                </div>
-
-                <div className="confirmedAppointments">
-                <div className="titleFilter">
-                    <h2>Completed Appointments</h2>
-                    <div className="btn-group">
-                    <button type="button" className="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-                        Filter
-                    </button>
-                    <ul className="dropdown-menu">
-                        <li><a className="dropdown-item" href="#">Date</a></li>
-                        <li><a className="dropdown-item" href="#">Purpose</a></li>
-                    </ul>
-                </div>
-                </div>
-                    <table className="table">
-                    <thead>
-                        <tr>
-                            <th scope="col">Date</th>
-                            <th scope="col">Time</th>
-                            <th scope="col">Period</th>
-                            <th scope="col">Purpose</th>
-                            <th scope="col">Requested by:</th>
-                            <th scope="col">More Info</th> 
-                        </tr>
-                    </thead>
-                    <tbody>
-                    <tr>
-                            <td>
-                                11/22/21
-                                {/*massSchedules.massDate*/}
-                            </td>
-                            <td>
-                                1:00
-                                {/*massSchedules.massDate*/}
-                            </td>
-                            <td>
-                                Pm
-                                {/*massSchedules.massDate*/}
-                            </td>
-                            <td>
-                                Burial
-                                {/*massSchedules.massDate*/}
-                            </td>
-                            <td>
-                                Navia Caspar
-                                {/*massSchedules.massDate*/}
-                            </td>
-                            <td>
-                                <button type="button" class="btn btn-info">Info</button>
-                            </td>
-                    </tr>
-                    </tbody>
-                    </table>
-                </div>
-            </div>
-
-            <div className="apppointmentCalendar">
-                <h3>Calendar</h3>
-                
-                <div className="calendar2">
-                    <div className='row'>
-                        <div className='calendar col-lg-4 col-md-6 me-5'>
-                            <DatePicker
-                                inline
-                                selected={startDate}
-                                onChange={(date) => setStartDate(date)}
-                                dateFormat="MMMM d, yyyy"
-                            />
-                        </div>
+            <div className="appointmentsView">    
+                <div className="Appointments">
+                    <div className="titleFilter">
+                        <h3>Pending Appointments:</h3>
+                            <div className='appointmentFilterDates'>
+                                <label className='me-2'><b></b></label>
+                                <DatePicker
+                                className='form-control'
+                                selected={selectedDate}
+                                onChange={date => setSelectedDate(date)}
+                                showYearDropdown
+                                />
+                            </div>
+                            <div className="btn-group">
+                                <button type="button" className="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Filter
+                                </button>
+                                <ul className="dropdown-menu">
+                                    <li><a className="dropdown-item" href="#">Date</a></li>
+                                    <li><a className="dropdown-item" href="#">Purpose</a></li>
+                                </ul>
+                            </div>
                     </div>
-                
-
-              <div className='slots2 col-lg-4 col-md-6'>
-                 <div>mu appear ang selected date</div>
-                <div className="form-check">
-                  <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1"/>
-                  <label className="form-check-label" htmlFor="flexRadioDefault1">sampleTime</label>
-                  <br/>
-                  <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2"/>
-                  <label className="form-check-label" htmlFor="flexRadioDefault2">sampleTime number 2</label>
-                  <br/>
-                  <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2"/>
-                  <label className="form-check-label" htmlFor="flexRadioDefault2">sampleTime number 2</label>
-                  <br/>
-                  <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2"/>
-                  <label className="form-check-label" htmlFor="flexRadioDefault2">sampleTime number 2</label>
-                  <br/>
-                  <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2"/>
-                  <label className="form-check-label" htmlFor="flexRadioDefault2">sampleTime number 2</label>
+                    <br/>
+                    <table className="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">Date</th>
+                                <th scope="col">Time</th>
+                                <th scope="col">Period</th>
+                                <th scope="col">Purpose</th>
+                                <th scope="col">Requested by:</th>
+                                <th scope="col">More Info</th> 
+                            </tr>
+                        </thead>
+                        <tbody> 
+                            {pendingAppointments.map((appointment, index) => (
+                                <tr key={index}>
+                                    <td>11/22/21</td>
+                                    <td>1:00</td>
+                                    <td>Pm</td>
+                                    <td>Burial</td>
+                                    <td>Navia Caspar</td>
+                                    <td><button type="button" className="btn btn-info">Info</button></td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
                 </div>
-              </div>
+
+                <div className="Appointments">
+                    <div className="titleFilter">
+                        <h3>Completed Appointments:</h3>
+                            <div className='appointmentFilterDates'>
+                                <label className='me-2'><b></b></label>
+                                <DatePicker
+                                className='form-control'
+                                selected={selectedDate}
+                                onChange={date => setSelectedDate(date)}
+                                showYearDropdown
+                                />
+                            </div>
+                            <div className="btn-group">
+                                <button type="button" className="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Filter
+                                </button>
+                                <ul className="dropdown-menu">
+                                    <li><a className="dropdown-item" href="#">Date</a></li>
+                                    <li><a className="dropdown-item" href="#">Purpose</a></li>
+                                </ul>
+                            </div>
+                    </div>
+                    <br/>
+                    <table className="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">Date</th>
+                                <th scope="col">Time</th>
+                                <th scope="col">Period</th>
+                                <th scope="col">Purpose</th>
+                                <th scope="col">Requested by:</th>
+                                <th scope="col">More Info</th> 
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>11/22/21</td>
+                                <td>1:00</td>
+                                <td>Pm</td>
+                                <td>Burial</td>
+                                <td>Navia Caspar</td>
+                                <td><button type="button" className="btn btn-info">Info</button></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
             </div>
-            <div className="buttonAreas col-md-12 d-grid gap-2 d-md-block">
-                <button type="submit" className="btn btn-success me-2">Select schedule</button>
-                <button type="reset" className="btn btn-danger me-2">Clear</button>
-            </div>
-            </div>
+
+            
 
             <Modal show={showModal} onHide={handleCloseModal}>
                 <Modal.Header closeButton>
