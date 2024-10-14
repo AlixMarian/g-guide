9c0debfaaf114eb068f5ac6c5a29912d3e3030af
NaviLeiru <sejismundoi123@gmail.com>
2024-10-14 14:21:53 +0800

    minor code fixes

--- src/components2/SEA.jsx
+++ src/components2/SEA.jsx
@@ -1,21 +1,10 @@
-import React, { useEffect, useState } from 'react';
-import {getMassList,addMassSchedule,updateMassSchedule,
-  deleteMassSchedule,getEventList,addEventSchedule,
-  updateEventSchedule,deleteEventSchedule,getAnnouncementList,
-  addAnnouncement,deleteAnnouncement,updateAnnouncement,
-  getPriestList
-} from '../components2/Services/seaServices';
-import { getAuth, onAuthStateChanged } from 'firebase/auth';
-import { Timestamp } from 'firebase/firestore';
-import { toast } from 'react-toastify';
-import '../churchCoordinator.css';
-import DatePicker from 'react-datepicker';
+
 
 export const SEA = () => {
   
   return (
     <>
-        <h1>Hello</h1>
+        <h1>Welcome ChurchCoordinator</h1>
     </>
   );
 };
