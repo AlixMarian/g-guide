ae5cb4a754aab0bda2ef845947092c36cfcd7695
NaviLeiru <sejismundoi123@gmail.com>
2024-11-27 14:00:02 +0800

    mapfiles folder added, some map files moved to folder

--- src/components/VisitaIglesia.jsx
+++ src/components/VisitaIglesia.jsx
@@ -3,11 +3,11 @@ import { useNavigate } from 'react-router-dom';
 import React, { useState, useEffect, useRef } from 'react';
 import { GoogleMap, LoadScript, DirectionsRenderer, Autocomplete, Marker } from '@react-google-maps/api';
 import { Offcanvas, Button, Form } from 'react-bootstrap';
-import { fetchChurchData } from '../components/churchDataUtils';
+import { fetchChurchData } from './mapfiles/churchDataUtils';
 // import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 // import { FaBars } from 'react-icons/fa'; // Drag Handle Icon
 import loadingGif from '../assets/Ripple@1x-1.0s-200px-200px.gif';
-import { handleMarkerClick, handleMapLoad, onZoomChanged } from '../components/churchDataUtils';
+import { handleMarkerClick, handleMapLoad, onZoomChanged } from './mapfiles/churchDataUtils';
 
 
 const containerStyle = {
